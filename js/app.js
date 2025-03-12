4bc46043dd5553548a08cb7001e24530c04a8c0c
Jayyy <myaloneaura@gmail.com>
2025-03-12 18:48:24 -0400

    5

--- js/app.js
+++ js/app.js
@@ -47,3 +47,33 @@ function addToDo(toDo, id, done, trash) {
   if (trash) {
     return;
   }
+ const DONE = done ? CHECK : UNCHECK;
+  const LINE = done ? LINE_THROUGH : "";
+
+  const text = `<li class="item">
+  <i class="fa ${DONE} complete" job="complete" id="${id}"></i>
+  <p class="text ${LINE}">${toDo}</p>
+  <i class="de fa fa-trash-o" job="delete" id="${id}"></i>
+</li>`;
+
+
+  const position = "beforeend";
+  list.insertAdjacentHTML(position, text); 
+}
+
+document.addEventListener("keyup", function (event) { 
+  if (event.keyCode == 13) {
+    const toDo = input.value;
+    if (toDo) {
+      addToDo(toDo, id, false, false);
+      LIST.push({
+        name: toDo,
+        id: id,
+        done: false,
+        trash: false,
+      });
+      localStorage.setItem("TODO", JSON.stringify(LIST));
+      id++;
+    }
+    input.value = "";
+  }
