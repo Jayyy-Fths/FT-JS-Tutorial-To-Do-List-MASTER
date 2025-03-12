1c59ded05f093fb15efd4616fbe43c39261ffa30
Jayyy <myaloneaura@gmail.com>
2025-03-12 18:47:36 -0400

    2

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 // CODE EXPLAINED channel
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+let LIST, id;
+
+let data = localStorage.getItem("TODO");
+
+if(data){
+    LIST = JSON.parse(data);
+    id = LIST.length;
+    loadList(LIST);
+}else{
+    LIST = [];
+    id = 0;
+}
